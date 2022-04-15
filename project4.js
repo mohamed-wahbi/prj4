c82cca8e549b499aa3915c2b84a4a32b2b1bb5b8
mohamed-wahbi <95344703+mohamed-wahbi@users.noreply.github.com>
2022-04-15 07:10:15 +0200

    Update project4.js

--- project4.js
+++ project4.js
@@ -22,4 +22,33 @@ mongela.style.background=localStorage.getItem("loun");
 
 }
 onload = lonh ();
+function abc (){
+if(screen.width<=600 || screen.height<=600){
+ 
+  document.getElementById("cadre").style.width="236px";
+  document.getElementById("cadre").style.height="224px";
+  document.getElementById("cadre").style.background="red";
+
+  document.getElementById("mongela").style.width="306px";
+  document.getElementById("mongela").style.height="234px";
+}
+}
+onload =new abc ();
+ function cba (){
+   if(screen.width<=300){ 
+      document.getElementById("cadre").style.width="236px";
+  document.getElementById("cadre").style.height="224px";
+  document.getElementById("cadre").style.background="green";
+
+  document.getElementById("mongela").style.width="306px";
+  document.getElementById("mongela").style.height="234px"; 
+  document.getElementById("ssd").className="w300"; 
+document.getElementById("mongela").className="w400";
+
+  }
+  
+
+
+ }
+ onload= new cba ();
 
